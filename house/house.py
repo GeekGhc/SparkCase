7ed8b4714bd8fa9c67ac435a6e92fbb6bbac95ff
GeekGhc <ghcgavin@sina.com>
2021-06-18 01:37:49 +0800

    house

--- house/house.py
+++ house/house.py
@@ -1,17 +1,18 @@
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import col
-from pyspark.sql.types import FloatType, DataType
+from pyspark.sql.types import FloatType
+from pyspark.ml.linalg import DenseVector
 
 # 初始化SparkSession
 spark = SparkSession.builder.master("local").appName("California Housing").config("spark.executor.memory",
                                                                                   "1gb").getOrCreate()
 
 # 读取数据并创建RDD
-rdd = spark.read.text('./california_housing/cal_housing.data').rdd
+dataRdd = spark.read.text('./california_housing/cal_housing.data').rdd
 # 读取数据的每个属性的定义并创建RDD
 headerRdd = spark.read.text('./california_housing/cal_housing.domain').rdd
 # 字符分隔数组
-rdd = rdd.map(lambda x: x[0].split(','))
+rdd = dataRdd.map(lambda x: x[0].split(','))
 # 转成结构化DataFrame
 df = rdd.map(lambda line: Row(longitude=line[0],
                               latitude=line[1],
@@ -25,8 +26,8 @@ df = rdd.map(lambda line: Row(longitude=line[0],
 
 
 # 字段类型转换
-def convertColumn(df, names, newType):
-    for name in names:
+def convertColumn(df, columns, newType):
+    for name in columns:
         df = df.withColumn(name, df[name].cast(newType))
     return df
 
@@ -36,11 +37,13 @@ columns = ['households', 'housingMedianAge', 'latitude', 'longitude', 'medianHou
            'totalBedRooms', 'totalRooms']
 # 字段类型转换
 df = convertColumn(df, columns, FloatType())
+
 # 统计所有建造年限各有多少房子
 # df.groupBy("housingMedianAge").count().sort("housingMedianAge", ascending=False).show()
 
 # 房价字段处理
-df = df.withColumn("medianHouseValue", col("medianHouseValue") / 100000)
+# df = df.withColumn("medianHouseValue", col("medianHouseValue") / 100000)
+
 # 添加 每个家庭的平均房间数、每个家庭的平均人数、卧室在总房间的占比
 df = df.withColumn("roomsPerHousehold", col("totalRooms") / col("households")).withColumn("populationPerHousehold",
                                                                                           col("population") / col(
@@ -56,4 +59,9 @@ df = df.select("medianHouseValue",
                "roomsPerHousehold",
                "populationPerHousehold",
                "bedroomsPerRoom")
+
+# 对象分成两部分：房价和一个包含其余属性的列表
+input_data = df.rdd.map(lambda x: (x[0], DenseVector(x[1:])))
+df = spark.createDataFrame(input_data, ["label", "features"])
+
 df.show()
